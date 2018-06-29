654361755dc168320e7fc1139c44a2dd9da8a0f3
Emma Dufresne <emmakdufresne@gmail.com>
2018-06-29 12:53:05 -0400

    Adding GUIs

--- VPDdemandclass.py
+++ VPDdemandclass.py
@@ -8,12 +8,17 @@ class VPDdemand:
     self.placevalues = builder.boolOut("VPDplacevalues",ZNAM = 0, ONAM = 1, HIGH = 0.1, on_update=self.place_voltages)
     #pv created to turn VPD voltages to 0   
     self.turnvoltageoff = builder.boolOut("turnoffVPD",ZNAM = 0, ONAM = 1, HIGH = 0.1, on_update=self.turnoff_VPD)
-   
-    self.dictionary = {}
-    self.dictionary[1] = ["file_name", "description"]
-    self.dictionary[2] = ["file_name", "description"]
-    self.activefile = 1
     #self.chooseFile = builder.longOut("VPD_setting", initial_value = self.activefile , on_update = self.request_change)
+    self.dictionary = {} 
+    self.activefile = 1 
+    #self.reload
+    #self.fileInUse
+    #self.listofstringIns
+    #self.maximum = 10  
+    #for i in range(self.maximum):
+      #self.listofstringIns.append(builder.stringIn("VPDfile"+str(i+1)))
+
+    
   def place_voltages(self,x):
     if x == 0:
       return
@@ -42,8 +47,30 @@ class VPDdemand:
       softioc.dbpf(self.boardList[BoardID].channels[chanID].calc.name+".HIGH", "300")
       softioc.dbpf(self.boardList[BoardID].channels[chanID].calc.name+".HIHI", "300")
 
-  #def request_change:
-    #return
+  """
+  def request_change(self,val):
+    if val not in self.dictionary:
+      print "Number does not correlate to file in dictionary"
+      return
+    self.activefile = val
+    self.fineInUse.set(self.dictionary[val][1])
+
+  when content of file is changed
+  def reload_dictionary(self,y=1):
+    if y == 0:
+      return 
+    self.dictionary.clear()
+    for i in range(len(self.listofstringIns)):
+      self.lsitofstringIns[i].set("")
+    f = pandas.read_csv('VPDprofile.csv')
+    for line in range(len(f)):
+      if line >= self.maximum:
+        return
+      filename = f['filename'][line]
+      description = f['description'][line]
+      self.dictionary[line+1] = filename,description]
+      self.listofstringIns[line].set(description)
+  """
 
   def turnoff_VPD(self,y):
     if y == 0:
